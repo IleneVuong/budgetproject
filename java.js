f035379a3e8ae08e259cd1d395703ea26c0a18ac
Ilene Vuong <ilenevuong2005@hotmail.com>
2023-02-07 15:48:07 -0800

    js

--- java.js
+++ java.js
@@ -15,7 +15,7 @@ let data = [
     [ 'Chemist', 56700 ],
     [ 'Civil Engineering Technician', 68250 ],
     [ 'Commercial Driver', 51450 ],
-    [ 'ComputerTechnician', 46200 ],
+    [ 'Computer Technician', 46200 ],
     [ 'Conserv./Environ. Science', 72450 ],
     [ 'Correctional Officer', 48300 ],
     [ 'Cosmetologist', 36750 ],
@@ -71,218 +71,59 @@ let data = [
     [ 'Wind EnergyTechnician', 56700 ]
 ]
     
-// let careers = 
-//     ['Accountant',
-//      'Advance Tractor/Trailer Driver',
-//      'Agricultural Engineer',
-//      'Architect',
-//      'Auto Tech/Mechanic', 
-//      'Aviation Tech. Mechanic', 
-//      'Biologist', 
-//      'Bus Driver',
-//      'Business Development Officer',
-//      'Business Manager Hotel etc.', 
-//      'CNC Manufacturing', 
-//      'Carpenter', 
-//      'Chef',
-//      'Chemist',
-//      'Civil Engineering Technician', 
-//      'Commercial Driver', 
-//      'ComputerTechnician', 
-//      'Conserv./Environ. Science',
-//      'Correctional Officer', 
-//      'Cosmetologist', 
-//      'Credit Union/Bank Manager', 
-//      'Daycare Director', 
-//      'Dentist', 
-//      'Detective',
-//      'Diesel Tech/Mechanic', 
-//      'Doctor/Physician',
-//      'Electrician', 
-//      'Electronic Engineer', 
-//      'EMT', 
-//      'Energy Management PG&E', 
-//      'Engineer', 
-//      'Fashion Designer', 
-//      'Fire Fighter',
-//      'Forest Ranger', 
-//      'Graphic/Media Designer', 
-//      'H/C HVAC', 
-//      'Highway Patrol', 
-//      'Home Inspector', 
-//      'Industrial Mechanic', 
-//      'Interior Designer', 
-//      'Investment Analyst', 
-//      'Lab Technician', 
-//      'Landscaper Horticulture',
-//      'Lawyer', 
-//      'Marketing/Sales Manager',
-//      'Media/Communications', 
-//      'Medical RepairTech.',
-//      'Military', 
-//      'Nurse', 
-//      'Nutitionist', 
-//      'Oceanographer', 
-//      'Pastor', 
-//      'PGBEATT Technician',
-//      'Pharmacist', 
-//      'Photographer', 
-//      'Physical Therapist', 
-//      'Pilot Commercial',  
-//      'Plumber', 
-//      'Police Officer', 
-//      'Principal', 
-//      'Probation Officer', 
-//      'Psychologist', 
-//      'Retail Sales Associate', 
-//      'Social Worker', 
-//      'Solar Energy Tech.', 
-//      'Teacher',
-//      'UPS/Fed Ex Driver', 
-//      'Veterinarian',
-//      'Welder/Metal Specialist',
-//      'Wind EnergyTechnician'
-// ];
-
-// let income = [
-//     55650,
-//     53550,
-//     56700,
-//     53550, 
-//     49350,
-//     50400,
-//     54600,
-//     37800,
-//     54600,
-//     61950,
-//     80850,
-//     47250,
-//     52500,
-//     56700,
-//     68250,
-//     51450,
-//     46200,
-//     72450,
-//     48300,
-//     36750,
-//     61950,
-//     37800,
-//     115500,
-//     60900, 
-//     55650, 
-//     147000,
-//     54600,
-//     75600,
-//     34650,
-//     106050,
-//     72450,
-//     63000,
-//     49350,
-//     52500,
-//     58800,
-//     63000,
-//     84000,
-//     56700,
-//     46200,
-//     49350,
-//     66150,
-//     42000,
-//     48300,
-//     86100,
-//     58800,
-//     45150,
-//     52500,
-//     55650,
-//     66150,
-//     45150,
-//     69300,
-//     50400,
-//     78750,
-//     105000,
-//     45150,
-//     72450,
-//     78750,
-//     52500,
-//     53550,
-//     93450,
-//     44100,
-//     77700,
-//     34650,
-//     50400,
-//     53550,
-//     52500,
-//     68250,
-//     82950,
-//     47250,
-//     56700 
-// ];
 
 let e = document.getElementById('temp');
-    let grossAnnual = 0;
-    let career = e.options[e.selectedIndex].text;
-    e.addEventListener('change', setIncome);
-
-    function setIncome(){
-        grossAnnual = e.value;
-        // document.getElementById("gr").innerHTML = filter(career);
-        document.getElementById("gr").innerHTML = grossAnnual;
-    }
-
-    for(job of data){
-        let newOption = document.createElement('option');
-        newOption.setAttribute('value', job[1]);
-        newOption.innerText = job[0];
-        e.appendChild(newOption)
-    }
-
-let filteredIncome = [];
-let a = [];
-// const gmi = [];
+let grossAnnual = 0;
 let gmi =0;
+let career = e.options[e.selectedIndex].text;
+e.addEventListener('change', setIncome);
+    
 
-// function filter() {
-//     for (var i = 0; i < careers.length; i++) {
-//         if (careers[i] == career) {
-//             filteredIncome = income[i];
-//             return filteredIncome;
-//             // console.log(filteredIncome);
-//             // gmi = income[i]/12;
-//             // console.log(Math.round(gmi));
-//             // a = i;
-//             // console.log(a);
-//         }
-//     }
-// }
+function setIncome(){
+    grossAnnual = e.value;
+    // document.getElementById("gr").innerHTML = filter(career);
+    document.getElementById("gr").innerHTML = grossAnnual;
+}
+
+for(job of data){
+    let newOption = document.createElement('option');
+    newOption.setAttribute('value', job[1]);
+    newOption.innerText = job[0];
+    e.appendChild(newOption)
+}
 
 let g = document.getElementById("temp");
 g.addEventListener('change', divide);
+g.addEventListener('change', reset);
+let mmhp = 0;
 
 function divide() {
     gmi = Math.round(grossAnnual/12);
     document.getElementById("monthly").innerHTML = gmi;
-    return gmi;
 }
-
-
-
-
 
 function taxes() {
-    ft = Math.round(gmi * .12);
-    st = Math.round(gmi * .07);
-    ss = Math.round(gmi * 0.062);
-    mc = Math.round(gmi * 0.0145);
-    sd = Math.round(gmi * 0.01);
-    ri = Math.round(gmi * 0.05);
-    mi = 180
-    td = ft + st + ss + mc + sd + ri + mi;
-    nmi = gmi - td;
-    console.log(ft, st, ss, mc, sd, ri, mi, td, nmi);
-    return(nmi);
+    let ft = Math.round(gmi * .12);
+    document.getElementById("ft-out").innerHTML = ft;
+    let st = Math.round(gmi * .07);
+    document.getElementById("st-out").innerHTML = st;
+    let ss = Math.round(gmi * 0.062);
+    document.getElementById("ss-out").innerHTML = ss;
+    let mc = Math.round(gmi * 0.0145);
+    document.getElementById("mc-out").innerHTML = mc;
+    let sd = Math.round(gmi * 0.01);
+    document.getElementById("sd-out").innerHTML = sd;
+    let ri = Math.round(gmi * 0.05);
+    document.getElementById("ri-out").innerHTML = ri;
+    let mi = 180
+    document.getElementById("mi-out").innerHTML = mi;
+    let td = ft + st + ss + mc + sd + ri + mi;
+    document.getElementById("td-out2").innerHTML = td;
+    let nmi = gmi - td;
+    document.getElementById("nmi-out").innerHTML = nmi; 
+    document.getElementById("gmi").innerHTML = gmi; 
 }
-taxes();
 
-let mmhp = 0;
 let t = document.getElementById("houseButton");
 t.addEventListener('click', housePayment);
 
@@ -292,10 +133,9 @@ function housePayment() {
     return mmhp;
 }
 
-let d = document.getElementById("temp");
-d.addEventListener('change', reset);
 function reset() {
-    mmhp=;
+    mmhp = 0;
     document.getElementById("paymentNumber").innerHTML = mmhp;
-    return mmhp;
 }
+
+t.addEventListener("click",taxes);
